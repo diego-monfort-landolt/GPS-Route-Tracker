5931bf97540295418eabfdd1e96e2b9e8b62a01c
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-10-22 19:48:41 +0200

    add alert and add color for tracking

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let map;
 let path = [];
 let watchId;
+let polyline;
 
 function initMap() {
     map = L.map('map').setView([0, 0], 2);
@@ -8,6 +9,7 @@ function initMap() {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    polyline = L.polyline([], { color: 'red' }).addTo(map);
 }
 
 function updateMap() {
@@ -19,14 +21,20 @@ function updateMap() {
 document.getElementById('startButton').addEventListener('click', () => {
     if (navigator.geolocation) {
         watchId = navigator.geolocation.watchPosition(position => {
-            const { latitude, longitude } = position.coords;
-            path.push({ latitude, longitude });
-            L.marker([latitude, longitude]).addTo(map);
-            updateMap();
-        });
-    } else {
-        alert('Geolocation is not supported by this browser.');
-    }
+          const { latitude, longitude } = position.coords;
+          path.push({ latitude, longitude });
+          L.marker([latitude, longitude]).addTo(map);
+          updateMap();
+      }, error => {
+          console.error(error);
+      }, {
+          enableHighAccuracy: true,
+          maximumAge: 0, // Ensure no caching
+          timeout: 5000
+      });
+  } else {
+      alert('Geolocation is not supported by this browser.');
+  }
 });
 
 document.getElementById('stopButton').addEventListener('click', () => {
