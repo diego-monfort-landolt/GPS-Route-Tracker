7e04535a95dfafe3787b8e11082a00a816690a52
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-10-22 19:38:22 +0200

    add new code

---
+++ script.js
@@ -0,0 +1,40 @@
+let map;
+let path = [];
+let watchId;
+
+function initMap() {
+    map = L.map('map').setView([0, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+
+function updateMap() {
+    const latlngs = path.map(p => [p.latitude, p.longitude]);
+    L.polyline(latlngs, { color: 'red' }).addTo(map);
+    map.setView(latlngs[latlngs.length - 1], 15);
+}
+
+document.getElementById('startButton').addEventListener('click', () => {
+    if (navigator.geolocation) {
+        watchId = navigator.geolocation.watchPosition(position => {
+            const { latitude, longitude } = position.coords;
+            path.push({ latitude, longitude });
+            L.marker([latitude, longitude]).addTo(map);
+            updateMap();
+        });
+    } else {
+        alert('Geolocation is not supported by this browser.');
+    }
+});
+
+document.getElementById('stopButton').addEventListener('click', () => {
+    if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+        alert('Das Tracking wurde gestoppt.');
+    }
+});
+
+initMap();
